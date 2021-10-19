e94181d8346d1cb1ffed615b186c3a88741cf04d
Lobsang <lobsangg7777@gmail.com>
2021-10-18 22:31:01 -0500

    falta import django_heroku en settings

--- ProyectCovid19/settings_prod.py
+++ ProyectCovid19/settings_prod.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
