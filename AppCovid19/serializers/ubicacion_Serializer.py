78eddb3164fe4cd020aefd98c9371d1e627718d7
Lobsang <lobsangg7777@gmail.com>
2021-10-20 19:41:29 -0500

    filtrar arreglados

--- AppCovid19/serializers/ubicacion_Serializer.py
+++ AppCovid19/serializers/ubicacion_Serializer.py
@@ -1,5 +1,7 @@
-from AppCovid19.models.ubicacion import Ubicacion
-from rest_framework              import serializers
+from AppCovid19.models import Ubicacion
+from rest_framework    import serializers
+from AppCovid19.models import Registro
+from AppCovid19.models import Seguimiento_de_cambios
 
 class UbicacionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,7 +9,10 @@ class UbicacionSerializer(serializers.ModelSerializer):
         fields = ['codigoDivipolaMunicipio', 'codigo_iso_pais', 'nombre_pais', 'codigo_divipola_departamento', 'nombre_departamento', 'nombre_municipio']
     
     def to_representation(self, obj):
-        ubicacion = Ubicacion.objects.get(codigoDivipolaMunicipio=obj.codigoDivipolaMunicipio)
+        ubicacion           = Ubicacion.objects.get(codigoDivipolaMunicipio=obj.codigoDivipolaMunicipio)
+        registro            = Registro.objects.get(codigo_divipola_municipio_fk=obj.codigoDivipolaMunicipio)
+        seguimiento_cambios = Seguimiento_de_cambios.objects.get(id_caso_fk=obj.codigoDivipolaMunicipio)
+
         return {
             'codigoDivipolaMunicipio'       : ubicacion.codigoDivipolaMunicipio,
             'codigo_iso_pais'               : ubicacion.codigo_iso_pais,
@@ -15,4 +20,28 @@ class UbicacionSerializer(serializers.ModelSerializer):
             'codigo_divipola_departamento'  : ubicacion.codigo_divipola_departamento,
             'nombre_departamento'           : ubicacion.nombre_departamento,
             'nombre_municipio'              : ubicacion.nombre_municipio,
+            'registro'                : {
+                'id_caso'                           :registro.id_caso,
+                'codigo_divipola_municipio_fk'      :registro.codigo_divipola_municipio_fk_id,
+                'fecha_notificacion'                :registro.fecha_notificacion,
+                'fecha_reporte'                     :registro.fecha_reporte,
+                'fecha_sintomas'                    :registro.fecha_sintomas,
+                'fecha_diagnostico_lab'             :registro.fecha_diagnostico_lab,
+                'edad'                              :registro.edad,
+                'unidad_de_medida_edad'             :registro.unidad_de_medida_edad,
+                'sexo'                              :registro.sexo,
+                'grupo_etnico'                      :registro.grupo_etnico,
+                'pertenencia_etnica'                :registro.pertenencia_etnica,
+                'fecha_recuperacion'                :registro.fecha_recuperacion,
+                'tipo_recuperacion'                 :registro.tipo_recuperacion                
+            },
+            'seguimiento'       :{
+                'id_evolucion'            : seguimiento_cambios.id_evolucion,
+                'id_caso_fk'              : seguimiento_cambios.id_caso_fk_id,
+                'ubicacion_caso'          : seguimiento_cambios.ubicacion_caso,
+                'estado'                  : seguimiento_cambios.estado,
+                'tipo_contagio'           : seguimiento_cambios.tipo_contagio,
+                'recuperado'              : seguimiento_cambios.recuperado,
+                'fecha_muerte'            : seguimiento_cambios.fecha_muerte,
+            }
         }
